6e23ed13aaf363f209eac4e356bc14e691cce93c
JoseCainan <josecainanlv@gmail.com>
2025-07-25 21:31:33 -0300

    atualização scroll e animações

---
+++ efeitos.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('.apresentacao', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  reset: true
+});
+
+ScrollReveal().reveal('.passo', {
+  duration: 1000,
+  origin: 'left',
+  distance: '40px',
+  interval: 200,
+  reset: true
+});
+
+ScrollReveal().reveal('.card--quem', {
+  duration: 1000,
+  origin: 'right',
+  distance: '50px',
+  reset: true
+});
